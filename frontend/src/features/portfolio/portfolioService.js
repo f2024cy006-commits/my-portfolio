17fa93f034743e00f3a3093ad04e67286995252c
maarijurrehman <maarijrana162@gmail.com>
2026-08-30 14:36:22 +0500

    Services Change

--- frontend/src/features/portfolio/portfolioService.js
+++ frontend/src/features/portfolio/portfolioService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = '/api'
+const API_URL = `${import.meta.env.VITE_API_URL}/api`
 
 const getPortfolio = async () => {
   const response = await axios.get(`${API_URL}/portfolio`)
